4d59502670a880cd98eddc75a325cf5b790d552a
Kacper Madej <kacper.madej@u2i.com>
2015-02-19 07:15:17 +0000

    Namespaced BlacklistedEmail, set table name to blacklisted_emails

--- lib/email_validation/blacklisted_email.rb
+++ lib/email_validation/blacklisted_email.rb
@@ -1,42 +1,45 @@
-class BlacklistedEmail < ActiveRecord::Base
+module EmailValidation
+  class BlacklistedEmail < ActiveRecord::Base
+    self.table_name = 'blacklisted_emails'
 
-  include Workflow
-  workflow_column :origin
+    include Workflow
+    workflow_column :origin
 
-  attr_accessible :email, :origin
+    attr_accessible :email, :origin
 
-  validates :origin, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^\s]+@[^\s]+\z/i }
+    validates :origin, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^\s]+@[^\s]+\z/i }
 
-  before_save :parse_email
+    before_save :parse_email
 
-  workflow do
-    state :bounce
-    state :unsubscribe
-  end
+    workflow do
+      state :bounce
+      state :unsubscribe
+    end
 
-  def self.parse_email_address email
-    email.gsub(/\+.*@/, "@")
-  end
+    def self.parse_email_address email
+      email.gsub(/\+.*@/, "@")
+    end
 
-  def self.exists? email
-    email = BlacklistedEmail.parse_email_address(email.to_s)
-    BlacklistedEmail.where(email: email).exists?
-  end
+    def self.exists? email
+      email = BlacklistedEmail.parse_email_address(email.to_s)
+      BlacklistedEmail.where(email: email).exists?
+    end
 
-  def self.bounced? email
-    email = BlacklistedEmail.parse_email_address(email.to_s)
-    BlacklistedEmail.where(email: email, origin: "bounce").exists?
-  end
+    def self.bounced? email
+      email = BlacklistedEmail.parse_email_address(email.to_s)
+      BlacklistedEmail.where(email: email, origin: "bounce").exists?
+    end
 
-  def self.unsubscribed? email
-    email = BlacklistedEmail.parse_email_address(email.to_s)
-    BlacklistedEmail.where(email: email, origin: "unsubscribe").exists?
-  end
+    def self.unsubscribed? email
+      email = BlacklistedEmail.parse_email_address(email.to_s)
+      BlacklistedEmail.where(email: email, origin: "unsubscribe").exists?
+    end
 
-  protected
+    protected
 
-  def parse_email
-    self.email = BlacklistedEmail.parse_email_address(self.email)
+    def parse_email
+      self.email = BlacklistedEmail.parse_email_address(self.email)
+    end
   end
 end
